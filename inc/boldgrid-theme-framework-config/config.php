7d636712515fc41f3e7f1c98c01e984a12ff7563
Tim <dev@tim.ph>
2016-05-31 13:39:41 -0400

    update generic footer configs.

--- inc/boldgrid-theme-framework-config/config.php
+++ inc/boldgrid-theme-framework-config/config.php
@@ -1,32 +1,50 @@
 <?php
 function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
-	/**
-	 * General Configs
-	 */
+
 	// Text domain.
 	$boldgrid_framework_configs['theme_name'] = 'boldgrid-florentine';
+
 	// Enable Sticky Footer.
 	$boldgrid_framework_configs['scripts']['boldgrid-sticky-footer'] = true;
+
 	// Enable typography controls.
 	$boldgrid_framework_configs['customizer-options']['typography']['enabled'] = true;
+
 	// Enable attribution links.
 	$boldgrid_framework_configs['temp']['attribution_links'] = true;
+
 	// Enable template wrapper.
 	$boldgrid_framework_configs['boldgrid-parent-theme'] = true;
+
 	// Specify the parent theme's name.
 	$boldgrid_framework_configs['parent-theme-name'] = 'prime';
-	// Select the footer template to use.
-	$boldgrid_framework_configs['template']['footer'] = '1';
+
 	// Select the header template to use.
 	$boldgrid_framework_configs['template']['header'] = 'generic';
-	// Assign menus, widgets, and actions to locations in generic header template.
+
+	// Select the footer template to use.
+	$boldgrid_framework_configs['template']['footer'] = 'generic';
+
+	// Assign Locations for Generic Header.
 	$boldgrid_framework_configs['template']['locations']['header'] = array(
-		'6' => array( '[action]boldgrid_site_identity' ),
-		'7' => array( '[menu]tertiary' ),
-		'8' => array( '[widget]boldgrid-widget-1', '[menu]secondary', '[action]boldgrid_primary_navigation' ),
-		'9' => array( '[menu]social' ),
-		'10' => array( '[widget]boldgrid-widget-2' ),
+		'1' => array( '[menu]tertiary' ),
+		'5' => array( '[menu]secondary' ),
+		'8' => array( '[action]boldgrid_site_identity', '[action]boldgrid_primary_navigation' ),
+		'11' => array( '[widget]boldgrid-widget-2' ),
+		'8' => array( '[action]' ),
 	);
+
+	// Assign Locations for Generic Header.
+	$boldgrid_framework_configs['template']['locations']['footer'] = array(
+		'6' => array( '[widget]boldgrid-widget-3', '[menu]footer_center', '[action]boldgrid_display_attribution_links' ),
+		'7' => array( '[menu]social' ),
+	);
+
+	// Social Menus.
+	$boldgrid_framework_configs['menu']['footer_menus'][] = 'social';
+
+	// Set the Call To Action Widget to be on Home Page Only.
+	$boldgrid_framework_configs['template']['call-to-action'] = 'home-only';
 
 	/**
 	 * Customizer Configs
